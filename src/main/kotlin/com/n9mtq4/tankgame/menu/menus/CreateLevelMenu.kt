39ac2357ada02d19c0196acb81c884590002d0c6
n9Mtq4 <bill.wresnahan@gmail.com>
2017-06-04 01:52:45 -0400

    brush sizes

--- src/main/kotlin/com/n9mtq4/tankgame/menu/menus/CreateLevelMenu.kt
+++ src/main/kotlin/com/n9mtq4/tankgame/menu/menus/CreateLevelMenu.kt
@@ -16,6 +16,7 @@ import com.n9mtq4.tankgame.menu.MenuManager
 import java.awt.Color
 import java.awt.Font
 import java.awt.Graphics
+import java.awt.event.KeyEvent
 import java.awt.event.MouseEvent
 
 /**
@@ -24,20 +25,20 @@ import java.awt.event.MouseEvent
  * @author Will "n9Mtq4" Bresnahan
  */
 
-typealias Point = Pair<Int, Int>
+private typealias Point = Pair<Int, Int>
 
 class CreateLevelMenu(menuManager: MenuManager) : Menu(menuManager) {
 	
 	companion object {
-		const val SCORE_PLACEHOLDER = "SCORE"
-		val PLACEHOLDER_FONT = Font("Verdana", Font.BOLD, 100 * GAME_SCALE)
+		val CONTROL_FONT = Font("Verdana", Font.BOLD, 10 * GAME_SCALE)
+		val CONTROL_FONT_SPACING = 2 * GAME_SCALE
 		val POINT_IDENTITY = Point(-1, -1)
 	}
 	
 	private var mousePressed = false
 	private var barrier = true
 	
-	private var brushSize = 1
+	private var brushRadius = 0
 	private var lastDragPoint = POINT_IDENTITY
 	
 	private var level = Level(LEVEL_WIDTH, LEVEL_HEIGHT, menuManager.gameMenu)
@@ -54,10 +55,14 @@ class CreateLevelMenu(menuManager: MenuManager) : Menu(menuManager) {
 		g.fillRect(0, 0, GAME_WIDTH * GAME_SCALE, SCORE_OFFSET * GAME_SCALE)
 		
 		g.color = Color.BLACK
-		g.font = PLACEHOLDER_FONT
+		g.font = CONTROL_FONT
 		
-		val height1 = g.font.getHeight(SCORE_PLACEHOLDER, g.frc)
-		g.drawString(SCORE_PLACEHOLDER, calcCenter(SCORE_PLACEHOLDER, g.font, g.frc), height1 + GameClass.SCORE_MARGIN_TOP)
+//		val height1 = g.font.getHeight(SCORE_PLACEHOLDER, g.frc)
+//		g.drawString(SCORE_PLACEHOLDER, calcCenter(SCORE_PLACEHOLDER, g.font, g.frc), height1 + GameClass.SCORE_MARGIN_TOP)
+		
+		val height1 = g.font.getHeight("ABCDEFG", g.frc)
+		g.drawString("Brush Radius: $brushRadius", 0, height1 + GameClass.SCORE_MARGIN_TOP)
+		g.drawString("[ to decrease size, ] to increase size", 0, 2 * (height1 + CONTROL_FONT_SPACING) + GameClass.SCORE_MARGIN_TOP)
 		
 	}
 	
@@ -67,7 +72,7 @@ class CreateLevelMenu(menuManager: MenuManager) : Menu(menuManager) {
 		
 	}
 	
-	fun checkBarrierStatus(x: Int, y: Int) = ignore {
+	private fun checkBarrierStatus(x: Int, y: Int) = ignore {
 		
 		val levelX = level.aGetRenderX(x.toDouble()).toInt()
 		val levelY = level.aGetRenderY(y.toDouble()).toInt()
@@ -77,7 +82,7 @@ class CreateLevelMenu(menuManager: MenuManager) : Menu(menuManager) {
 		
 	}
 	
-	fun onDrag(x: Int, y: Int) {
+	private fun onDrag(x: Int, y: Int) {
 		
 		if (lastDragPoint != POINT_IDENTITY) {
 			
@@ -117,14 +122,42 @@ class CreateLevelMenu(menuManager: MenuManager) : Menu(menuManager) {
 		
 	}
 	
-	fun toggleLevelBarrierWindowPos(x: Int, y: Int) = ignore {
+	private fun toggleLevelBarrierWindowPos(x: Int, y: Int) = ignore {
 		
-		val levelX = level.aGetRenderX(x.toDouble()).toInt()
+		getAllPointsInBrushFromMouse(x, y).forEach { (levelX, levelY) -> 
+			if (barrier) level[levelX, levelY] = Tile.ClosedTile(levelX, levelY, menuManager.gameMenu)
+			else level[levelX, levelY] = Tile.OpenTile(levelX, levelY, menuManager.gameMenu)
+		}
+		
+/*		val levelX = level.aGetRenderX(x.toDouble()).toInt()
 		val levelY = level.aGetRenderY(y.toDouble()).toInt()
 		
 		if (barrier) level[levelX, levelY] = Tile.ClosedTile(levelX, levelY, menuManager.gameMenu)
-		else level[levelX, levelY] = Tile.OpenTile(levelX, levelY, menuManager.gameMenu)
+		else level[levelX, levelY] = Tile.OpenTile(levelX, levelY, menuManager.gameMenu)*/
 		
+	}
+	
+	private fun getAllPointsInBrushFromMouse(x: Int, y: Int): List<Point> {
+		
+		val pList = mutableListOf<Point>()
+		
+		// get reference point
+		val levelX = level.aGetRenderX(x.toDouble()).toInt()
+		val levelY = level.aGetRenderY(y.toDouble()).toInt()
+		
+		// add a square with this level points
+		for (by in -brushRadius..brushRadius) (-brushRadius..brushRadius).mapTo(pList) { bx -> Point(bx + levelX, by + levelY) }
+		
+		return pList
+		
+	}
+	
+	private fun decBrush() {
+		if (brushRadius > 0) brushRadius--
+	}
+	
+	private fun incBrush() {
+		if (brushRadius < 10) brushRadius++
 	}
 	
 	override fun mousePressed(e: MouseEvent?) {
@@ -142,6 +175,13 @@ class CreateLevelMenu(menuManager: MenuManager) : Menu(menuManager) {
 	
 	override fun mouseDragged(e: MouseEvent?) {
 		e?.let { onDrag(it.x, it.y) }
+	}
+	
+	override fun keyPressed(e: KeyEvent?) {
+		when (e?.keyCode) {
+			KeyEvent.VK_OPEN_BRACKET -> decBrush()
+			KeyEvent.VK_CLOSE_BRACKET -> incBrush()
+		}
 	}
 	
 }
