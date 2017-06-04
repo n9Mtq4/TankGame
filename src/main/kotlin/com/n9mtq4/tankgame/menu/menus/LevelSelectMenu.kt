0351534df6e9a04f646b13e292ce977e98ffb0d6
n9Mtq4 <bill.wresnahan@gmail.com>
2017-06-04 15:31:49 -0400

    placeholder level selection menu

--- src/main/kotlin/com/n9mtq4/tankgame/menu/menus/LevelSelectMenu.kt
+++ src/main/kotlin/com/n9mtq4/tankgame/menu/menus/LevelSelectMenu.kt
@@ -2,6 +2,8 @@ package com.n9mtq4.tankgame.menu.menus
 
 import com.n9mtq4.tankgame.menu.Menu
 import com.n9mtq4.tankgame.menu.MenuManager
+import java.awt.Color
+import java.awt.Graphics
 
 /**
  * Created by will on 5/29/17 at 8:53 PM.
@@ -10,6 +12,15 @@ import com.n9mtq4.tankgame.menu.MenuManager
  */
 class LevelSelectMenu(menuManager: MenuManager) : Menu(menuManager) {
 	
-	
+	override fun draw(g: Graphics) {
+		
+		super.draw(g)
+		
+		g.color = Color.BLACK
+		g.drawString("Level select placeholder", 20, 40)
+		
+		g.drawString("Push ESC to go back", 20, 60)
+		
+	}
 	
 }
